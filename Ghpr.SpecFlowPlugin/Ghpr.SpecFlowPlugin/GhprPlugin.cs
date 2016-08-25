bea02bcae1dcd0ff2c2480d802e50c3abf1cd3b2
Evgeniy Kosjakov <42_@mail.ru>
2016-08-25 12:56:50 +0300

    initial

---
+++ Ghpr.SpecFlowPlugin/Ghpr.SpecFlowPlugin/GhprPlugin.cs
@@ -0,0 +1,30 @@
+﻿using TechTalk.SpecFlow.Infrastructure;
+using TechTalk.SpecFlow.Plugins;
+
+namespace Ghpr.SpecFlowPlugin
+{
+    public class GhprPlugin : IRuntimePlugin
+    {
+        public void Initialize(RuntimePluginEvents runtimePluginEvents, RuntimePluginParameters runtimePluginParameters)
+        {
+            Log.Write("Initialize");
+
+            //var oc = new ObjectContainer();
+            //oc.RegisterTypeAs<GhprTestExecutionEngine, ITestExecutionEngine>();
+            //runtimePluginEvents.RaiseRegisterGlobalDependencies(oc);
+
+            runtimePluginEvents.CustomizeGlobalDependencies += CustomizeGlobalDependencies;
+
+        }
+
+        private void CustomizeGlobalDependencies(object sender, CustomizeGlobalDependenciesEventArgs e)
+        {
+            CustomizeDependencies(e);
+        }
+
+        public void CustomizeDependencies(CustomizeGlobalDependenciesEventArgs eventArgs)
+        {
+            eventArgs.ObjectContainer.RegisterTypeAs<GhprTestExecutionEngine, ITestExecutionEngine>();
+        }
+    }
+}
---
+++ Ghpr.SpecFlowPlugin/Ghpr.SpecFlowPlugin/GhprTestExecutionEngine.cs
@@ -0,0 +1,58 @@
+﻿using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Bindings;
+using TechTalk.SpecFlow.Infrastructure;
+
+namespace Ghpr.SpecFlowPlugin
+{
+    public class GhprTestExecutionEngine : ITestExecutionEngine
+    {
+        public void OnTestRunStart()
+        {
+            Log.Write("Test run start");
+        }
+
+        public void OnTestRunEnd()
+        {
+            Log.Write("Test run end");
+        }
+
+        public void OnFeatureStart(FeatureInfo featureInfo)
+        {
+            Log.Write("Feature start");
+        }
+
+        public void OnFeatureEnd()
+        {
+            Log.Write("Feature end");
+        }
+
+        public void OnScenarioStart(ScenarioInfo scenarioInfo)
+        {
+            Log.Write("Scenario start");
+        }
+
+        public void OnAfterLastStep()
+        {
+            Log.Write("Last step");
+        }
+
+        public void OnScenarioEnd()
+        {
+            Log.Write("Scenario end");
+        }
+
+        public void Step(StepDefinitionKeyword stepDefinitionKeyword, string keyword, string text, string multilineTextArg,
+            Table tableArg)
+        {
+            Log.Write("Step");
+        }
+
+        public void Pending()
+        {
+            Log.Write("Pending");
+        }
+
+        public FeatureContext FeatureContext { get; }
+        public ScenarioContext ScenarioContext { get; }
+    }
+}
