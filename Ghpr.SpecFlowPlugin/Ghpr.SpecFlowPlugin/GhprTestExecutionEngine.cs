428c259529390aa8305fac3d5ba73624b9d88b46
Evgeniy Kosjakov <42_@mail.ru>
2017-04-21 17:11:30 +0300

    lock and reporter manager added

--- Ghpr.SpecFlowPlugin/Ghpr.SpecFlowPlugin/GhprTestExecutionEngine.cs
+++ Ghpr.SpecFlowPlugin/Ghpr.SpecFlowPlugin/GhprTestExecutionEngine.cs
@@ -1,7 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Ghpr.Core;
 using Ghpr.Core.Common;
-using Ghpr.Core.Enums;
 using Ghpr.Core.Interfaces;
 using Ghpr.Core.Utils;
 using NUnit.Framework;
@@ -19,11 +20,11 @@ namespace Ghpr.SpecFlowPlugin
     public class GhprTestExecutionEngine : ITestExecutionEngine
     {
         private readonly TestExecutionEngine _engine;
-        private readonly Reporter _reporter;
         private FeatureInfo _currentFeatureInfo;
         private ITestRun _currentTestRun;
         
         private readonly Log _l;
+        private static readonly object Lock = new object();
 
         public GhprTestExecutionEngine(
             IStepFormatter stepFormatter, 
@@ -52,98 +53,106 @@ namespace Ghpr.SpecFlowPlugin
                 stepErrorHandlers,
                 bindingInvoker);
             
-            _reporter = new Reporter(TestingFramework.SpecFlow);
-
-            _l = new Log(_reporter.Settings.OutputPath, "plugin.txt");
-            //_l.Write("constructor");
+            _l = new Log(ReporterManager.OutputPath, $"plugin_{Guid.NewGuid()}.txt");
+            _l.Write("constructor");
         }
         
         public void OnTestRunStart()
         {
-            _engine.OnTestRunStart();
-            _reporter.RunStarted();
-
-            OutputHelper.Initialize();
-
-            //_l.Write("run started");
+            lock (Lock)
+            {
+                ReporterManager.RunStarted();
+                OutputHelper.Initialize();
+                _engine.OnTestRunStart();
+                _l.Write("run started");
+            }
         }
 
         public void OnTestRunEnd()
         {
-            _engine.OnTestRunEnd();
-            OutputHelper.Flush();
-            OutputHelper.Dispose();
-            _reporter.RunFinished();
-
-            //_l.Write("run finished");
+            lock (Lock)
+            {
+                OutputHelper.Flush();
+                OutputHelper.Dispose();
+                ReporterManager.RunFinished();
+                _engine.OnTestRunEnd();
+                _l.Write("run finished");
+            }
         }
 
         public void OnFeatureStart(FeatureInfo featureInfo)
         {
-            _currentFeatureInfo = featureInfo;
-            _engine.OnFeatureStart(featureInfo);
-
-            //_l.Write($"feature '{featureInfo.Title}' started");
+            lock (Lock)
+            {
+                _currentFeatureInfo = featureInfo;
+                _engine.OnFeatureStart(featureInfo);
+                _l.Write($"feature '{featureInfo.Title}' started");
+            }
         }
 
         public void OnFeatureEnd()
         {
-            _engine.OnFeatureEnd();
-            OutputHelper.Flush();
-
-            //_l.Write("feature finished");
+            lock (Lock)
+            {
+                OutputHelper.Flush();
+                _engine.OnFeatureEnd();
+                _l.Write("feature finished");
+            }
         }
         
         public void OnScenarioStart(ScenarioInfo scenarioInfo)
         {
-            _engine.OnScenarioStart(scenarioInfo);
-            OutputHelper.WriteFeature(_currentFeatureInfo);
-            OutputHelper.WriteScenario(scenarioInfo);
-
-            //_engine.ScenarioContext.StepContext.StepInfo.MultilineText;
-
-            _currentTestRun = new TestRun
+            lock (Lock)
             {
-                Name = scenarioInfo.Title,
-                FullName = $"{TestContext.CurrentContext.Test.ClassName}.{_currentFeatureInfo.Title}",
-                Categories = scenarioInfo.Tags
-            };
-            _reporter.TestStarted(_currentTestRun);
-            
-            //_l.Write($"scenario '{scenarioInfo.Title}' started");
+                OutputHelper.WriteFeature(_currentFeatureInfo);
+                OutputHelper.WriteScenario(scenarioInfo);
+                var className = TestContext.CurrentContext.Test.ClassName;
+                var testName = TestContext.CurrentContext.Test.Name;
+                var names = className.Split('.').ToList();
+                if (names.Count >= 2)
+                {
+                    names.RemoveAt(names.Count - 1);
+                }
+                className = string.Join(".", names);
+                _currentTestRun = new TestRun
+                {
+                    Name = scenarioInfo.Title,
+                    FullName = $"{className}.{_currentFeatureInfo.Title}.{testName}",
+                    Categories = scenarioInfo.Tags
+                };
+                ReporterManager.TestStarted(_currentTestRun);
+                _engine.OnScenarioStart(scenarioInfo);
+                _l.Write($"scenario '{scenarioInfo.Title}' started");
+            }
         }
 
         public void OnScenarioEnd()
         {
-            _engine.OnScenarioEnd();
-            
-            var te = _engine.ScenarioContext?.TestError;
-            _currentTestRun.Output = OutputHelper.GetOutput();
-            _currentTestRun.Result = te == null ? "Passed" : (te is AssertionException ? "Failed" : "Error");
-            _currentTestRun.TestMessage = te?.Message ?? "";
-            _currentTestRun.TestStackTrace = te?.StackTrace ?? "";
-
-            _reporter.TestFinished(_currentTestRun);
-
-            //_l.Write("scenario finished");
-
-            OutputHelper.Flush();
+            lock (Lock)
+            {
+                var te = _engine.ScenarioContext?.TestError;
+                _currentTestRun.Output = OutputHelper.GetOutput();
+                _currentTestRun.Result = te == null ? "Passed" : (te is AssertionException ? "Failed" : "Error");
+                _currentTestRun.TestMessage = te?.Message ?? "";
+                _currentTestRun.TestStackTrace = te?.StackTrace ?? "";
+                ReporterManager.TestFinished(_currentTestRun);
+                _engine.OnScenarioEnd();
+                OutputHelper.Flush();
+                _l.Write("scenario finished");
+            }
         }
 
         public void OnAfterLastStep()
         {
             _engine.OnAfterLastStep();
-
-            //_l.Write("after last step");
+            _l.Write("after last step");
         }
 
         public void Step(StepDefinitionKeyword stepDefinitionKeyword, string keyword, string text, string multilineTextArg,
             Table tableArg)
         {
             _engine.Step(stepDefinitionKeyword, keyword, text, multilineTextArg, tableArg);
-            _currentTestRun.FullName += text;
             OutputHelper.WriteStep(text);
-            //_l.Write($"step: '{text}'");
         }
 
         public void Pending()
