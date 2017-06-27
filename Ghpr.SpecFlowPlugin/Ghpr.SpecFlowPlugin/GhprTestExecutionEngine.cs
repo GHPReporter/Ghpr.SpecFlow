d3133292443d8ae3d4096cefa93ffcdf39c2be3d
Evgeniy Kosjakov <42_@mail.ru>
2017-06-27 20:10:15 +0300

    to v0.5 with screenshots

--- Ghpr.SpecFlowPlugin/Ghpr.SpecFlowPlugin/GhprTestExecutionEngine.cs
+++ Ghpr.SpecFlowPlugin/Ghpr.SpecFlowPlugin/GhprTestExecutionEngine.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using Ghpr.Core;
 using Ghpr.Core.Common;
 using Ghpr.Core.Interfaces;
+using Ghpr.Core.Utils;
 using NUnit.Framework;
 using TechTalk.SpecFlow;
 using TechTalk.SpecFlow.Bindings;
@@ -111,10 +112,11 @@ namespace Ghpr.SpecFlowPlugin
                     names.RemoveAt(names.Count - 1);
                 }
                 className = string.Join(".", names);
-                _currentTestRun = new TestRun
+                var fullName = $"{className}.{_currentFeatureInfo.Title}.{testName}";
+                var name = scenarioInfo.Title;
+                var guid = GuidConverter.ToMd5HashGuid(_currentTestRun.FullName).ToString();
+                _currentTestRun = new TestRun(guid, name, fullName)
                 {
-                    Name = scenarioInfo.Title,
-                    FullName = $"{className}.{_currentFeatureInfo.Title}.{testName}",
                     Categories = scenarioInfo.Tags
                 };
                 ReporterManager.TestStarted(_currentTestRun);
@@ -132,6 +134,7 @@ namespace Ghpr.SpecFlowPlugin
                 _currentTestRun.Result = te == null ? "Passed" : (te is AssertionException ? "Failed" : "Error");
                 _currentTestRun.TestMessage = te?.Message ?? "";
                 _currentTestRun.TestStackTrace = te?.StackTrace ?? "";
+                _currentTestRun.Screenshots = ScreenHelper.GetScreenshots();
                 ReporterManager.TestFinished(_currentTestRun);
                 _engine.OnScenarioEnd();
                 //OutputHelper.Flush();
