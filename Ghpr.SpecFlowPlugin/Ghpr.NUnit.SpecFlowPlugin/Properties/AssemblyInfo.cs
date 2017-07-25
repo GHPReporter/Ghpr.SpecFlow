126b1a1b749ba1611ec51ec633c9441e8160fb64
Evgeniy Kosjakov <42_@mail.ru>
2017-07-25 17:38:00 +0300

    srceenshots support added, config transformation added

--- Ghpr.SpecFlowPlugin/Ghpr.NUnit.SpecFlowPlugin/Properties/AssemblyInfo.cs
+++ Ghpr.SpecFlowPlugin/Ghpr.NUnit.SpecFlowPlugin/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using TechTalk.SpecFlow.Plugins;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.5.0.6")]
-[assembly: AssemblyFileVersion("0.5.0.6")]
+[assembly: AssemblyVersion("0.5.0.7")]
+[assembly: AssemblyFileVersion("0.5.0.7")]
 
 [assembly: RuntimePlugin(typeof(GhprNUnitSpecFlowPlugin))]
--- Ghpr.SpecFlowPlugin/GhprMSTest.SpecFlowPlugin/GhprMSTestSpecFlowScreenHelper.cs
+++ Ghpr.SpecFlowPlugin/GhprMSTest.SpecFlowPlugin/GhprMSTestSpecFlowScreenHelper.cs
@@ -1,20 +1,64 @@
 ﻿// ReSharper disable InconsistentNaming
+
+using System;
 using System.Collections.Generic;
+using System.IO;
+using Ghpr.Core;
+using Ghpr.Core.Common;
+using Ghpr.Core.Helpers;
 using Ghpr.Core.Interfaces;
+using Ghpr.Core.Utils;
 using GhprSpecFlow.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace GhprMSTest.SpecFlowPlugin
 {
     public class GhprMSTestSpecFlowScreenHelper : IGhprSpecFlowScreenHelper
     {
+        private readonly TestContext _testContext;
+
+        public GhprMSTestSpecFlowScreenHelper()
+        {
+        }
+
+        public GhprMSTestSpecFlowScreenHelper(TestContext testContext)
+        {
+            _testContext = testContext;
+        }
+
         public void SaveScreenshot(byte[] screenBytes)
         {
-            throw new System.NotImplementedException();
+            var guid = _testContext.Properties["TestGuid"]?.ToString();
+            var fullName = GhprMSTestSpecFlowHelper.GetFullNameForGuid(_testContext);
+            var testGuid = guid != null ? Guid.Parse(guid) : GuidConverter.ToMd5HashGuid(fullName);
+            var fullPath = Path.Combine(ReporterManager.OutputPath, Paths.Folders.Tests, testGuid.ToString(), Paths.Folders.Img);
+            var screenshotName = ScreenshotHelper.SaveScreenshot(fullPath, screenBytes, DateTime.Now);
+            var count = 0;
+            var screenKey = ScreenshotHelper.GetScreenKey(count);
+            while (_testContext.Properties[screenKey] != null)
+            {
+                count++;
+                screenKey = ScreenshotHelper.GetScreenKey(count);
+            }
+
+            _testContext.Properties.Add(screenKey, screenshotName);
         }
 
         public List<ITestScreenshot> GetScreenshots()
         {
-            throw new System.NotImplementedException();
+            var screenshots = new List<ITestScreenshot>();
+            var count = 0;
+            var screenKey = ScreenshotHelper.GetScreenKey(count);
+            while (_testContext.Properties[screenKey] != null)
+            {
+                var screenshotName = _testContext.Properties[screenKey].ToString();
+                screenshots.Add(new TestScreenshot(screenshotName));
+
+                count++;
+                screenKey = ScreenshotHelper.GetScreenKey(count);
+            }
+
+            return screenshots;
         }
     }
 }
