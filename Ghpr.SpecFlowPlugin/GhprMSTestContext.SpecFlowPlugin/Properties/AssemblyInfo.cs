8f78ac005983f741fdac90e5cfde61e115198fad
Evgeniy Kosjakov <42_@mail.ru>
2017-12-14 16:26:05 +0300

    update to v0.8

--- Ghpr.SpecFlowPlugin/GhprMSTestContext.SpecFlowPlugin/Properties/AssemblyInfo.cs
+++ Ghpr.SpecFlowPlugin/GhprMSTestContext.SpecFlowPlugin/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using TechTalk.SpecFlow.Infrastructure;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.0.0")]
-[assembly: AssemblyFileVersion("0.7.0.0")]
+[assembly: AssemblyVersion("0.8.0.0")]
+[assembly: AssemblyFileVersion("0.8.0.0")]
 
 [assembly: GeneratorPlugin(typeof(GhprMSTestTestContextSpecFlowPlugin))]
